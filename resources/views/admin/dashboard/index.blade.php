0ea9e3895fe85ecaa875a8eba44bfb8c3af24b80
mozessinaga <akunlogis007@gmail.com>
2024-06-08 23:06:18 +0700

    icon user dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -59,7 +59,7 @@
 
     <div class="col-md-3">
         <div class="info-box">
-            <span class="info-box-icon bg-info elevation-1"><i class="fas fa-envelope"></i></span>
+            <span class="info-box-icon bg-info elevation-1"><i class="fas fa-user"></i></span>
 
             <div class="info-box-content">
                 <span class="info-box-text">User</span>
